6a8568237e7a79c4039c43e5e1a0b78cdd3b0c45
GabDestroyer <gabrielboivin@hotmail.com>
2014-02-26 21:48:06 -0500

    Afe commande en courant 2 level

---
+++ Remise/Simulation/AFE/AFEsimplepara.m
@@ -0,0 +1,18 @@
+%On déclare les variables
+%Inductance de source ac
+Lac = 0.0020;
+
+%Tension AC
+Vac = 2000;
+%Fréquence réseau
+Freq = 50;
+%Pas de simulation
+Tpas = 1e-5;
+%angle theta du courant
+Phase = 300;
+%Valeur max du courant
+Iac = 2000;
+%Résistance charge
+Rac = 2.5;
+
+
