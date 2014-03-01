3c3b55a9ab60d9a35ca24f4a5eaf95eb8e04bd4f
GabDestroyer <gabrielboivin@hotmail.com>
2014-02-28 20:29:40 -0500

    avancement AFE

--- Remise/Simulation/AFE/AFEsimplepara.m
+++ Remise/Simulation/AFE/AFEsimplepara.m
@@ -1,6 +1,6 @@
 %On déclare les variables
 %Inductance de source ac
-Lac = 0.0020;
+Lac = 0.0030;
 
 %Tension AC
 Vac = 2000;
@@ -9,10 +9,12 @@ Freq = 50;
 %Pas de simulation
 Tpas = 1e-5;
 %angle theta du courant
-Phase = 300;
-%Valeur max du courant
-Iac = 2000;
+Phase = 45;
+%Valeur max du courant: La relation avec la tension est U = Imax/cos(theta) 
+Iac = 3460;
 %Résistance charge
 Rac = 2.5;
+%Seuil hystérésis
+hys = 0.1;
 
 
