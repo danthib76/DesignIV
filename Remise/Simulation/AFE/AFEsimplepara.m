aea82b77b455946e927368cef4679770be70d673
Daniel Thibodeau <danthib76@gmail.com>
2014-03-01 23:49:28 -0500

    Ajustements préliminaires
    Ajout valeur inductances/tension/load value

--- Remise/Simulation/AFE/AFEsimplepara.m
+++ Remise/Simulation/AFE/AFEsimplepara.m
@@ -2,7 +2,7 @@ clear;
 clc;
 %On déclare les variables
 %Inductance de source ac
-Lac = 0.0010;
+Lac =  8.1487e-04;
 
 
 %Tension AC
@@ -10,11 +10,11 @@ Vac = 2000;
 %Fréquence réseau
 Freq = 50;
 %Pas de simulation
-Tpas = 1e-4;
+Tpas = 5e-5;
 %Déphasage du courant avec la tension
 Phase = 45;
 %Résistance charge
-Rac = 2.5;
+Rac = 1.2;
 %Seuil hystérésis
 hys = 0.0001;
 %tension de reférence dc
