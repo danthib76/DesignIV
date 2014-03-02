071da166d22221f969ce15afc8884204090dd15a
GabDestroyer <gabrielboivin@hotmail.com>
2014-03-02 03:04:28 -0500

    Ajustement
    Ajustement de la charge/ Ajustement pour fonctionnement en facteur de
    puissance unitaire

--- Remise/Simulation/AFE/AFEsimplepara.m
+++ Remise/Simulation/AFE/AFEsimplepara.m
@@ -1,7 +1,7 @@
 clear;
 clc;
 %On déclare les variables
-%Inductance de source ac
+%Inductance de source ac L = (0.16pu/(100*pi))*1.6
 Lac =  8.1487e-04;
 
 
@@ -12,12 +12,11 @@ Freq = 50;
 %Pas de simulation
 Tpas = 5e-5;
 %Déphasage du courant avec la tension
-Phase = 45;
-%Résistance charge
-Rac = 1.2;
+Phase = 0;
+%Résistance charge obtenu par la puissance moyenne R = 5000/(2.7MW/5000)
+Rch = 9.26;
 %Seuil hystérésis
 hys = 0.0001;
 %tension de reférence dc
 Vdc = 5000;
-
 
