111e38c8a2dd5f8b9d4f449a9f144a9b6ba63ee0
GabDestroyer <gabrielboivin@hotmail.com>
2014-03-21 19:11:15 -0400

    Modif DCP/DCN
    Changement du filtre pour garder une fréquence de commutation inférieure
    à 1000Hz. Mise a jour du doc sur la validation des résultats.

--- Remise/Simulation/DCP-DCN/DCP_DCN_Para.m
+++ Remise/Simulation/DCP-DCN/DCP_DCN_Para.m
@@ -6,7 +6,8 @@ clc;
 Tpas = 1e-5;
 %La fréquence fondamentale
 Freq = 50;
-%La fréquence du calcul de la moyenne de tension
+%La fréquence du calcul de la moyenne de tension (sensibilité depend du pas
+%de calcul)
 ech = 3000;
 %tension des sources du bus dc
 Vdc = 2500;
@@ -15,22 +16,27 @@ Lcoup = 1e-6;
 %Valeurs des électroaimants
 Rch = 0.28;
 Lch = 0.1;
+%Valeurs des snubbers
+Rs = 50000;
+Cs = 0.5;
+%Résistance IGBT
+Ron =10e-3;
 
 %Les paramètres du PI
 P = 1.5611;
 I = 24.6;
 
 %Les paramètres du filtre
-%[KLP,TLP] = ellip(2,0.001,80,400*Tpas);
+[KLP,TLP] = ellip(2,0.01,40,1000*(Tpas/2),'low');
 
-%Les paramètres du filtre (Code de discretisation de filtre venant de
-%VIarouge)
-Trip = .001;
-% Continuous Low Pass filter (ripple)
-LPrip = tf([1],[Trip,1]);
-% Discretization of Low Pass filter in z with Tstep
-LPripdisc=c2d(LPrip,Tpas);
-% Calcul des coeff du Low Pass filter discret de simulink
-[NLPripd,DLPripd,Ts]=tfdata(LPripdisc, 'v');
-KLP=NLPripd(2);
-TLP=DLPripd(2);
+% %Les paramètres du filtre (Code de discretisation de filtre venant de
+% %VIarouge)
+% Trip = .001;
+% % Continuous Low Pass filter (ripple)
+% LPrip = tf([1],[Trip,1]);
+% % Discretization of Low Pass filter in z with Tstep
+% LPripdisc=c2d(LPrip,Tpas);
+% % Calcul des coeff du Low Pass filter discret de simulink
+% [NLPripd,DLPripd,Ts]=tfdata(LPripdisc, 'v');
+% KLP=NLPripd(2);
+% TLP=DLPripd(2);
